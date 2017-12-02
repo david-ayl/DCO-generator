d509de2913650b556d311dfb9ca0debc6a07a287
dvdtsr <tessier.dvd@gmail.com>
2017-12-02 21:46:59 +0100

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+var $         = require("jquery");
+var loader    = require("./modules/loader.js");
+var resp_menu = require("./modules/responsive_menu.js");
+
+
+$(document).ready(function() {
+
+  loader();
+  resp_menu();
+
+});
